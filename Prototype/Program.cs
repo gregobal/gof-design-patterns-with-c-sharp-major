f904cb83ed246d67fd65f79a2d828df5680a1574
Gregory Balabanov <810411@mail.ru>
2023-04-04 15:45:25 +0500

    Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,34 @@
+﻿using Prototype;
+
+var prototype = new ConcretePrototype(1);
+var clone1 = prototype.Clone();
+var clone2 = prototype.Clone();
+
+Console.WriteLine(clone1.Id == clone2.Id);
+
+namespace Prototype
+{
+    public abstract class Prototype
+    {
+        public int Id { get; }
+
+        public Prototype(int id)
+        {
+            this.Id = id;
+        }
+
+        public abstract Prototype Clone();
+    }
+
+    internal class ConcretePrototype : Prototype
+    {
+        public ConcretePrototype(int id) : base(id)
+        {
+        }
+
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype(Id);
+        }
+    }
+}
