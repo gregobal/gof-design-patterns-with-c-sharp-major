52e07a869ef154589d1441bc3d9ae9142e017480
Gregory Balabanov <810411@mail.ru>
2023-04-04 16:02:15 +0500

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using SingletonNS;
+
+var instance1 = Singleton.Instance();
+var instance2 = Singleton.Instance();
+
+Console.WriteLine(instance1 == instance2);
+Console.WriteLine(instance1.Equals(instance2));
+
+namespace SingletonNS
+{
+    public class LazyThreadSafeSingleton
+    {
+        private LazyThreadSafeSingleton()
+        {
+        }
+
+        public static LazyThreadSafeSingleton Instance() => Holder.instance;
+
+        private static class Holder
+        {
+            static Holder()
+            {
+            }
+
+            internal static readonly LazyThreadSafeSingleton instance = new LazyThreadSafeSingleton();
+        }
+    }
+    
+    public class Singleton
+    {
+        private static Singleton? _instance;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            return _instance ??= new Singleton();
+        }
+    }
+}
