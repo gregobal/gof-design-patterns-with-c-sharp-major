ad2c5c8ad0e5528ac5aa9d87b9245629da44e1e5
Gregory Balabanov <810411@mail.ru>
2023-04-06 14:47:49 +0500

    Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,59 @@
+﻿using DecoratorNS;
+
+Component component = new ConcreteComponent();
+Decorator decoratorA = new ConcreteDecoratorA();
+Decorator decoratorB = new ConcreteDecoratorB();
+
+decoratorA.Component = component;
+decoratorA.Operation();
+
+decoratorB.Component = component;
+decoratorB.Operation();
+
+namespace DecoratorNS
+{
+    public abstract class Component
+    {
+        public abstract void Operation();
+    }
+    
+    public abstract class Decorator: Component
+    {
+        public Component? Component { protected get; set; }
+
+        public override void Operation()
+        {
+            Component?.Operation();
+        }
+    }
+
+    internal class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Concrete Component state");
+        }
+    }
+
+    internal class ConcreteDecoratorA : Decorator
+    {
+        string extraState = "Some extra state";
+
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine(extraState);
+        }
+    }
+    
+    internal class ConcreteDecoratorB : Decorator
+    {
+        void extraBehavior () => Console.WriteLine("Extra Behavior");
+
+        public override void Operation()
+        {
+            base.Operation();
+            extraBehavior();
+        }
+    }
+}
