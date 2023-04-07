fa1ce348df1ed2282a515f820b6d185aad2cf084
Gregory Balabanov <810411@mail.ru>
2023-04-07 11:49:08 +0500

    Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,31 @@
+﻿using ProxyNS;
+
+Subject subject = new Proxy();
+subject.Request();
+
+namespace ProxyNS
+{
+    public abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    internal class ConcreteSubject: Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Concrete Subject");
+        }
+    }
+
+    internal class Proxy : Subject
+    {
+        private ConcreteSubject? _subject;
+
+        public override void Request()
+        {
+            _subject ??= new ConcreteSubject();
+            _subject.Request();
+        }
+    }
+}
