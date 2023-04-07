e2de48bcb73fef7e92897d6fb2636ac646ba9d25
Gregory Balabanov <810411@mail.ru>
2023-04-07 22:23:44 +0500

    Command

---
+++ Command/Program.cs
@@ -0,0 +1,59 @@
+﻿using CommandNS;
+
+var receiver = new Receiver();
+Command command = new ConcreteCommand(receiver);
+var invoker = new Invoker();
+
+invoker.StoreCommand(command);
+invoker.ExecuteCommand();
+
+namespace CommandNS
+{
+    public class Invoker
+    {
+        Command Command;
+
+        public void StoreCommand(Command command)
+        {
+            Command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            Command.Execute();
+        }
+    }
+    
+    public abstract class Command
+    {
+        protected Receiver Receiver;
+
+        public Command(Receiver receiver)
+        {
+            Receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+
+    public class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Receive");
+        }
+    }
+
+    internal class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Receiver receiver):
+            base(receiver)
+        {
+        }
+
+        public override void Execute()
+        {
+            Receiver.Action();
+        }
+    }
+}
