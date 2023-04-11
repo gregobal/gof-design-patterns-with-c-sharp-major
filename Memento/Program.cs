1ef7eb9ba611f593160669e473d698253dab5ffb
Gregory Balabanov <810411@mail.ru>
2023-04-11 11:29:18 +0500

    Memento

---
+++ Memento/Program.cs
@@ -0,0 +1,52 @@
+﻿using MementoNS;
+
+var originator = new Originator();
+originator.State = "on";
+Console.WriteLine(originator);
+
+var caretaker = new Caretaker();
+caretaker.Memento = originator.CreateMemento();
+
+originator.State = "off";
+Console.WriteLine(originator);
+
+originator.SetMemento(caretaker.Memento);
+Console.WriteLine(originator);
+
+namespace MementoNS
+{
+    public class Memento
+    {
+        public string State { get; private set; }
+
+        public Memento(string state)
+        {
+            State = state;
+        }
+    }
+
+    internal class Caretaker
+    {
+        public Memento Memento { get; set; }
+    }
+
+    internal class Originator
+    {
+        public string State { get; set; }
+
+        public void SetMemento(Memento memento)
+        {
+            State = memento.State;
+        }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(State);
+        }
+
+        public override string ToString()
+        {
+            return $"Originator state is {State}";
+        }
+    }
+}
