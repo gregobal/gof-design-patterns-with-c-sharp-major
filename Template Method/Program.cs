58a507fa270cbd259b5501985188bd3878eef613
Gregory Balabanov <810411@mail.ru>
2023-04-13 18:43:06 +0500

    TemplateMethod

---
+++ Template Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using TemplateMethod;
+
+AbstractClass instance = new ConcreteClass();
+instance.TemplateMethod();
+
+namespace TemplateMethod
+{
+    public abstract class AbstractClass
+    {
+        protected abstract void PrimitiveOperation1();
+        protected abstract void PrimitiveOperation2();
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+        }
+    }
+
+    class ConcreteClass: AbstractClass
+    {
+        protected override void PrimitiveOperation1()
+        {
+            Console.WriteLine("PrimitiveOperation1");
+        }
+
+        protected override void PrimitiveOperation2()
+        {
+            Console.WriteLine("PrimitiveOperation2");
+        }
+    }
+}
