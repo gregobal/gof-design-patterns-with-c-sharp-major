aefaec8c54fc9a1ef51d34aaf647763ceb04edbc
Gregory Balabanov <810411@mail.ru>
2023-04-13 18:25:29 +0500

    Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,48 @@
+﻿using StrategyNS;
+
+Context context;
+
+context = new Context(new ConcreteStrategyA());
+context.ContextInterface();
+
+context = new Context(new ConcreteStrategyB());
+context.ContextInterface();
+
+namespace StrategyNS
+{
+    public abstract class Strategy
+    {
+        public abstract void AlgorithmInterface();
+    }
+
+    public class Context
+    {
+        private Strategy _strategy;
+
+        public Context(Strategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void ContextInterface()
+        {
+            _strategy.AlgorithmInterface();
+        }
+    }
+
+    internal class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("ConcreteStrategyA");
+        }
+    }
+    
+    internal class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("ConcreteStrategyB");
+        }
+    }
+}
